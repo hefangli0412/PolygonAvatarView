2315efbfd16a0f8a1cf902ff8ca40fd490b95989
hefangli <hefangli0412@gmail.com>
2015-04-15 14:10:19 -0700

    Initial Commit

---
+++ Polygon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Polygon
+//
+//  Created by Hefang Li on 4/15/15.
+//  Copyright (c) 2015 hefang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Polygon/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Polygon
+//
+//  Created by Hefang Li on 4/15/15.
+//  Copyright (c) 2015 hefang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
